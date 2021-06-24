fc58c7c0b33570f8146c4b44be7c2a711a0593f8
ErfanPY <erfanPy>
2021-06-24 14:41:07 +0430

    compare rooms

--- scrape/compare_rooms.py
+++ scrape/compare_rooms.py
@@ -9,18 +9,17 @@ def main():
 
     with get_db_connection() as conn:
         query = """
-            SELECT romUUID, romID, avlBasePrice,avlDAte, avlDiscountPrice, romName, rom_htlID
+            SELECT romUUID, romID, avlBasePrice,avlDate, avlDiscountPrice, romName, rom_htlID
             FROM tblRooms
             INNER JOIN tblAvailabilityInfo ON romID = avl_romID
-
-            GROUP BY romID
+            WHERE NOT romUUID IS NULL
             ;
         """
-        rows = custom(query)
+        rows = custom(query, conn)
         
         for i, row in enumerate(rows):
 
-            rowIdentifier = '|'.join(['' or row['romUUID'], row['avlDate']])
+            rowIdentifier = '|'.join([' ' or row['romUUID'], str(row['avlDate'])])
             other_room_index = rowIdentifier_rowIndex.get(rowIdentifier)
             
             if other_room_index is None or row['romUUID'] is None:
@@ -29,37 +28,38 @@ def main():
             
             del rowIdentifier_rowIndex[rowIdentifier]
 
-            compare_rooms(row, rows[other_room_index])
+            compare_rooms(row, rows[other_room_index], conn)
     
-    add_single_available_rooms(rowIdentifier_rowIndex)
+        add_single_available_rooms(rowIdentifier_rowIndex, conn)
 
 
-def compare_rooms(room1, room2):       
+def compare_rooms(room1, room2, conn):       
     if not room1['avlBasePrice'] == room2['avlBasePrice']:
         alrType = 'P'
         alrInfo = {
             room1['romID']: room1['avlBasePrice'],
             room2['romID']: room2['avlBasePrice'],
         }
-    if not room1['avlDiscountPrice'] == room2['avlDiscountPrice']:
+    elif not room1['avlDiscountPrice'] == room2['avlDiscountPrice']:
         alrType = 'D'
         alrInfo = {
             room1['romID']: room1['avlDiscountPrice'],
             room2['romID']: room2['avlDiscountPrice'],
         }
-    
-    insert_select_id(table='', key_value={
+    else:
+        return
+    insert_select_id(table='tblAlert', key_value={
         'alrType': alrType,
         'alrRoomUUID': room1['romUUID'],
-        'alrInfo': json(alrInfo)
-    }, id_field=None, identifier_condition=None)
+        'alrInfo': json.dumps(alrInfo)
+    }, id_field=None, identifier_condition=None, conn=conn)
  
 
-def add_single_available_rooms(rowIdentifier_rowIndex):
-    for rowIdentifier, _ in rowIdentifier_rowIndex.items():
-        romUUID, index = rowIdentifier.split('|')
+def add_single_available_rooms(rowIdentifier_rowIndex, conn):
+    for rowIdentifier, index in rowIdentifier_rowIndex.items():
+        romUUID, _ = rowIdentifier.split('|')
 
-        if romUUID == "":
+        if romUUID == " ":
             singlity_msg = "No mathcing room found on other hotel."
         else:
             singlity_msg = "Not alailable on this date."
@@ -68,11 +68,11 @@ def add_single_available_rooms(rowIdentifier_rowIndex):
             rows[index]['romID']: singlity_msg
         }
     
-        insert_select_id(table='', key_value={
+        insert_select_id(table='tblAlert', key_value={
             'alrType': 'R',
             'alrRoomUUID': romUUID,
-            'alrInfo': json(alrInfo)
-        }, id_field=None, identifier_condition=None)
+            'alrInfo': json.dumps(alrInfo)
+        }, id_field=None, identifier_condition=None, conn=conn)
 
 if __name__ == "__main__":
     main()        
