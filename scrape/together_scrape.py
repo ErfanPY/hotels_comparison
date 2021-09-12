58cff4befd07d2f3b7a3d4c703252ad37cc4899e
erfan <parsyab1@gmail.com>
2021-09-12 05:00:21 +0430

    add START_DAY_OFFSET env variable, save last scraped day, add more logging

--- scrape/together_scrape.py
+++ scrape/together_scrape.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import logging
+import os
 
 from scrape.db_util import (
     custom,
@@ -8,15 +9,17 @@ from scrape.db_util import (
 
 from scrape.alibaba.scraper import (
     TO_SCRAPE_CITIES,
+    START_DAY_OFFSET,
     city_ids,
-    get_city_hotels as alibaba_get_city_hotels,
     get_search_session_id,
-    scrape_hotel as alibaba_scrape_hotel
+    scrape_hotel as alibaba_scrape_hotel,
+    get_city_hotels as alibaba_get_city_hotels,
 ) 
 
 from scrape.snapp_trip.scraper import (
     get_city_hotels as snapp_get_city_hotels,
-    scrape_hotel as snapptrip_scrape_hotel
+    scrape_hotel as snapptrip_scrape_hotel,
+    en_fa_cities
 )
 
 from scrape.compare_rooms import main as compare_scrapes
@@ -24,32 +27,55 @@ from scrape.compare_rooms import main as compare_scrapes
 
 logger = logging.getLogger(__name__)
 
+START_DAY_OFFSET = os.environ.get("ALIBABA_START_DAY", 0)
+scrape_stat_path = "scrape_stat/"+'-'.join(TO_SCRAPE_CITIES)
+
+if not os.path.exists("scrape_stat"):
+    os.mkdir("scrape_stat")
+
+if START_DAY_OFFSET == 0 and os.path.exists(scrape_stat_path):
+    with open(scrape_stat_path) as f:
+        START_DAY_OFFSET = int(f.readline().strip())
+
 
 def main():
     visited_snapp_hotel = []
 
-    for day_offset in range(30):
+    for day_offset in range(START_DAY_OFFSET, 30):
+        logger.info(f"Day: {day_offset}")
+
         for city_name in TO_SCRAPE_CITIES:
+            logger.info(f"City: {city_name}")
+
             uuid_hotels = defaultdict(list)
 
-            for hotel in s_get_city_hotels(city_name, day_offset):
+            snapptrip_hotels = s_get_city_hotels(en_fa_cities[city_name], day_offset)
+            for hotel in snapptrip_hotels:
                 hotel_uuid = hotel_to_uuid(hotel)
 
-                if uuid and uuid in visited_snapp_hotel:
+                if hotel_uuid and hotel_uuid in visited_snapp_hotel:
                     continue
 
-                uuid_hotels[uuid].append(hotel)
-                visited_snapp_hotel.append(uuid)
-
-            for hotel in a_get_city_hotels(city_name, day_offset):
+                uuid_hotels[hotel_uuid].append(hotel)
+                visited_snapp_hotel.append(hotel_uuid)
+            
+            alibaba_hotels = a_get_city_hotels(city_name, day_offset)
+            for hotel in alibaba_hotels:
                 uuid_hotels[hotel_to_uuid(hotel)].append(hotel)
                 
             for i, (uuid, hotels) in enumerate(uuid_hotels.items()):
-                print(f"{city_name}: {i}/{len(hotels)}", end="\r")
 
                 for hotel in hotels:
                     scrape_hotel(hotel)
-                
+
+                logger.info(f"Hotels: {i}/{len(uuid_hotels)}")
+
+        with open(scrape_stat_path, 'w') as f:
+            f.write(str(day_offset+1))
+
+    with open(scrape_stat_path, 'w') as f:
+        f.write("0")
+
     compare_scrapes()
 
 
@@ -97,7 +123,9 @@ def hotel_to_uuid(hotel: dict) -> str:
     with get_db_connection() as conn:
         uuids = custom(query, [hotel['faName']], conn=conn)
     
-    return uuids[0]
+    uuid = uuids[0].get('htlUUID')
+    
+    return uuid
 
 
 def scrape_hotel(hotel: dict) -> int:
