ad48b42695022e538a8fe4c1ecc5d7edded885f9
erfan <parsyab1@gmail.com>
2021-09-16 11:14:50 +0430

    info with null when other site doesn't exist

--- api/parse_data_utils.py
+++ api/parse_data_utils.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import json
 
 
 def group_alerts(database_result, verbose):
@@ -8,15 +9,16 @@ def group_alerts(database_result, verbose):
     else:
         dc_indexs = {}
         for alert in database_result:
+            alert['alrInfo'] = json.loads(alert['alrInfo'])
             alert_data = alert_to_dict(alert)
 
-            dc_key = alert['alrDateTime']+alert['alrCrawlTime']
+            dc_key = f"{alert['alrDateTime']}-{alert['alrCrawlTime']}"
             dc_i = dc_indexs.get(dc_key)
             if dc_i is None:
                 dc_indexs[dc_key] = len(result_data)
                 result_data.append({
-                    "date": alert['alrDateTime'],
-                    "crawlStartTime": alert['alrCrawlTime'],
+                    "date": str(alert['alrDateTime'].date()),
+                    "crawlStartTime": str(alert['alrCrawlTime']),
                     "alerts": alert_data
                 })
             else:
@@ -39,13 +41,13 @@ def alert_to_dict(alert, verbose=False):
     bese_price = alert['alrInfo']['base_price']
     discount_price = alert['alrInfo']['discount_price']
 
-    alibaba_id = alert['alrA_romID']
-    snapptrip_id = alert['alrS_romID']
+    alibaba_id = str(alert['alrA_romID'])
+    snapptrip_id = str(alert['alrS_romID'])
 
     res = {
         alert['alrRoomUUID']: {
             "type": alert_type,
-            "detected-at": alert['alrDateTime'],
+            "detected-at": str(alert['alrDateTime']),
             "alibaba": {
                 "base_price": bese_price.get(alibaba_id),
                 "discount_amount": discount_price.get(alibaba_id),
@@ -54,7 +56,7 @@ def alert_to_dict(alert, verbose=False):
             "snapptrip": {
                 "base_price": bese_price.get(snapptrip_id),
                 "discount_amount": discount_price.get(snapptrip_id),
-                "name": alert['bName'],
+                "name": alert['sName'],
 
             }
         }
@@ -78,12 +80,14 @@ def verbose_alerts_to_dict(alerts):
 
     city_index = {}
     for alert in alerts:
+        alert['alrInfo'] = json.loads(alert['alrInfo'])
+
         i = city_index.get(alert['htlCity'])
         if i is None:
+            i = len(result)
             result.append({
                 alert['htlCity']: nested_dict()
             })
-            i = len(result)
             city_index[alert['htlCity']] = i
 
         result[i][alert['htlCity']][alert['htlUUID']].update(alert_to_dict(alert))
@@ -94,41 +98,56 @@ def verbose_alerts_to_dict(alerts):
 def group_infos(infos):
     groups = []
     dc_index = {} # dc = date_crawlDate
+    nested_dict = lambda: defaultdict(nested_dict)
   
     for info in infos:
-        dc_key = info["avlDate"]+info["avlCrawlTime"]
+        dc_key = f'{info["avlDate"]} {info["avlCrawlTime"]}'
         i = dc_index.get(dc_key)
-        site = "alibaba" if info['htlFrom'][0].lower() == "a" else "snapptrip"
-
+        site, other_site = "alibaba", "snapptrip" if info['htlFrom'][0].lower() == "a" else "snapptrip", "alibaba"
+        
         if i is None:
             i = len(groups)
             dc_index[dc_key] = i
-            groups.append({
-                "date": info["avlDate"],
-                "crawlStartTime": info["avlCrawlTime"],
-                info['htlCity']: {
-                    site: { # TODO:
-                        "lowest-price": {
-                            "amount": 1111,
-                            "room-name": "ROOM_NAME_ALIBABA_n",
-                            "room-uid": "ROOM_UID_n"
-                        },
-                        "room-count": 10,
-                        "hotel": {
-                            "fa": "HOTEL_NAME_ALIBABA_FA_n",
-                            "en": "HOTEL_NAME_ALIBABA_EN_n"
-                        }
-                    }
+            groups.append(nested_dict())
+            groups[i]["date"] = str(info["avlDate"])
+            groups[i]["crawlStartTime"] = str(info["avlCrawlTime"])
+            groups[i][info['htlCity']][info['htlUUID']][site] = info_to_site(info)
+            groups[i][info['htlCity']][info['htlUUID']][other_site] = { 
+                "lowest-price": {
+                    "amount": None,
+                    "room-name": None,
+                    "room-uid": None
                 },
-            })
+                "room-count": 0,
+                "hotel": {
+                    "fa": None,
+                    "en": None
+                }
+            }
+            
         else:
-            # TODO:
-            groups[i][info['htlCity']][info['htlUUID']]
+            prev_hotel = groups[i][info['htlCity']][info['htlUUID']].get(site)
+            if prev_hotel["room-count"] == 0:
+                groups[i][info['htlCity']][info['htlUUID']][site] = info_to_site(info)
+            else:
+                low_p = groups[i][info['htlCity']][info['htlUUID']][site]['lowest-price']['amount']
+                count = groups[i][info['htlCity']][info['htlUUID']][site]['room-count']
+                if info['avlDiscountPrice'] < low_p:
+                    groups[i][info['htlCity']][info['htlUUID']][site] = info_to_site(info)
+                groups[i][info['htlCity']][info['htlUUID']][site]['room-count'] = count+1
     return groups
 
-# avlInsertionDate, 
-# avlBasePrice, 
-# avlDiscountPrice, 
-# avlDate,
-# htlFaName, htlEnName, htlCity, htlFrom, htlUUID
-# romName, romMealPlan
+
+def info_to_site(info):
+    return { 
+        "lowest-price": {#TODO
+            "amount": info['avlDiscountPrice'],
+            "room-name": info['romName'],
+            "room-uid": info["romUUID"]
+        },
+        "room-count": 1, #TODO
+        "hotel": {
+            "fa": info["htlFaName"],
+            "en": info["htlEnName"]
+        }
+    }
